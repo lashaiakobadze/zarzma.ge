23b17847cfee1241d9b0894d9e3853e872d97f3e
Lasha <lashaiakobadze98@gmail.com>
2023-06-05 21:28:27 +0400

    style header for mob

--- layout/header/Header.tsx
+++ layout/header/Header.tsx
@@ -1,30 +1,33 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import Image from "next/image";
-import useTranslation from 'next-translate/useTranslation';
+import useTranslation from "next-translate/useTranslation";
 import style from "./Header.module.css";
 import NavigationItem from "@/components/shared/navItem/NavItem";
 import Link from "next/link";
 import { Howl } from "howler";
+import MobileContext from "@/contexts/MobileContext";
 
 const BASE_URL = process.env.dataUrl;
 
 const Header: React.FC = () => {
-  const { t, lang } = useTranslation('common');
+  const { t, lang } = useTranslation("common");
+  const { isMobile } = useContext(MobileContext);
 
   const dropdownItems: {
     label: string;
     href: string;
   }[] = [
-    { label: t('photos'), href: "/gallery/albums" },
-    { label: t('videos'), href: "/gallery/videos" },
-    { label: t('frescoes'), href: "/gallery/frescoes" },
-    { label: t('icons'), href: "/gallery/icons" },
+    { label: t("photos"), href: "/gallery/albums" },
+    { label: t("videos"), href: "/gallery/videos" },
+    { label: t("frescoes"), href: "/gallery/frescoes" },
+    { label: t("icons"), href: "/gallery/icons" },
   ];
+  const playSound = `${BASE_URL}/Zarzma/Chants/ufalo_shegviwyalen.mp3`;
+
 
   const [isPlaying, setIsPlaying] = useState(false);
   const [sound, setSound] = useState<Howl | null>(null);
-  const playSound =
-    `${BASE_URL}/Zarzma/Chants/ufalo_shegviwyalen.mp3`;
+  const [isOpen, setIsOpen] = useState(false);
 
   const initializeSound = () => {
     if (!sound) {
@@ -51,58 +54,102 @@ const Header: React.FC = () => {
     }
   };
 
+  const handleClickHamburger = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <header className={style.header}>
-      <a href={`/${lang}`}>
-        <Image
-          src="/main_assets/logo-zarzma.svg"
-          alt="logo-zarzma"
-          width={43}
-          height={47}
-        />
-      </a>
+    <>
+      {isMobile ? (
+        <header className={style.header}>
+          <a href={`/${lang}`}>
+            <Image
+              src="/main_assets/logo-zarzma.svg"
+              alt="logo-zarzma"
+              width={32}
+              height={34}
+            />
+          </a>
 
-      <nav className={style.header__container}>
-        <div className={style.header__item} onClick={handleClick}>
-          <Image
-            src={!isPlaying ? '/main_assets/play.svg' :  "/main_assets/stop.svg"}
-            alt="play-button"
-            width={24}
-            height={24}
-          />
-        </div>
+          <div className={`${style.header__item} ${style.header__itemCenter}`}>
+            <Link href={"/"} legacyBehavior>
+              <h1
+                className={`${style.header__title} ${style.header__titleMob}`}
+              >
+                ზარზმის მამათა მონასტერი
+              </h1>
+            </Link>
+          </div>
 
-        <div className={style.header__item}>
-          <NavigationItem
-            href="/gallery"
-            label={t('gallery')}
-            dropdownItems={dropdownItems}
-          />
-        </div>
+          <button
+            className={`${style.hamburger} ${isOpen ? style.hamburgerOpen : ""}`}
+            onClick={handleClickHamburger}
+          >
+            <span className={style.line}></span>
+            <span className={style.line}></span>
+            <span className={style.line}></span>
+          </button>
+        </header>
+      ) : (
+        <header className={style.header}>
+          <a href={`/${lang}`}>
+            <Image
+              src="/main_assets/logo-zarzma.svg"
+              alt="logo-zarzma"
+              width={43}
+              height={47}
+            />
+          </a>
 
-        <div className={style.header__item}>
-          <NavigationItem href="/chants" label={t('chants')} />
-        </div>
+          <nav className={style.header__container}>
+            <div className={style.header__item} onClick={handleClick}>
+              <Image
+                src={
+                  !isPlaying ? "/main_assets/play.svg" : "/main_assets/stop.svg"
+                }
+                alt="play-button"
+                width={24}
+                height={24}
+              />
+            </div>
 
-        <div className={`${style.header__item} ${style.header__itemCenter}`}>
-          <Link href={"/"} legacyBehavior>
-            <h1 className={style.header__title}>ზარზმის მამათა მონასტერი</h1>
-          </Link>
-        </div>
+            <div className={style.header__item}>
+              <NavigationItem
+                href="/gallery"
+                label={t("gallery")}
+                dropdownItems={dropdownItems}
+              />
+            </div>
 
-        <div className={style.header__item}>
-          <NavigationItem href="/publications" label={t('publications')} />
-        </div>
+            <div className={style.header__item}>
+              <NavigationItem href="/chants" label={t("chants")} />
+            </div>
 
-        <div className={style.header__item}>
-          <NavigationItem href="/handicraft" label={t('handicraft')} />
-        </div>
+            <div
+              className={`${style.header__item} ${style.header__itemCenter}`}
+            >
+              <Link href={"/"} legacyBehavior>
+                <h1 className={style.header__title}>
+                  ზარზმის მამათა მონასტერი
+                </h1>
+              </Link>
+            </div>
 
-        <div className={style.header__item}>
-          <NavigationItem href="/about" label={t('about')} />
-        </div>
-      </nav>
-    </header>
+            <div className={style.header__item}>
+              <NavigationItem href="/publications" label={t("publications")} />
+            </div>
+
+            <div className={style.header__item}>
+              <NavigationItem href="/handicraft" label={t("handicraft")} />
+            </div>
+
+            <div className={style.header__item}>
+              <NavigationItem href="/about" label={t("about")} />
+            </div>
+          </nav>
+        </header>
+      )}
+    </>
   );
 };
 
