1bec513ad702e0cac1c87cda268fe137257579dd
Lasha <lashaiakobadze98@gmail.com>
2023-06-07 17:18:00 +0400

    style album page for mob

--- components/albums/Albums.tsx
+++ components/albums/Albums.tsx
@@ -4,45 +4,90 @@ import { Album } from "@/pages/models/album.interface";
 import PageTitle from "../shared/page-title/PageTitle";
 import AlbumsSlider from "./albumsSlider/AlbumsSlider";
 import useTranslation from "next-translate/useTranslation";
+import Link from "next/link";
 
 interface AlbumsProps {
   albums: Album[];
   loading: boolean;
+  isMobile: boolean;
 }
 
-const Albums: React.FC<AlbumsProps> = ({ albums, loading }) => {
-  const { t, lang } = useTranslation('common');
+const BASE_URL = process.env.dataUrl + "/";
+
+const Albums: React.FC<AlbumsProps> = ({ albums, loading, isMobile }) => {
+  const { t } = useTranslation("common");
 
   return (
     <>
       <div className={style.albums}>
         <div className={style.albumsHead}>
-          <h2 className={style.albumsTitle}>ფოტო გალერეა</h2>
-          <div className={style.albumsItems}>
+          {!isMobile && <h2 className={style.albumsTitle}>ფოტო გალერეა</h2>}
+          <div
+            className={`${style.albumsItems} ${
+              isMobile ? style.albumsItemsMob : ""
+            }`}
+          >
             <img
-              className={style.albumsImg}
+              className={`${style.albumsImg} ${
+                isMobile ? style.albumsImgMob : ""
+              }`}
               src="/main_assets/png/Albums-img-1.png"
               alt="albums"
             />
             <img
-              className={style.albumsImg}
+              className={`${style.albumsImg} ${
+                isMobile ? style.albumsImgMob : ""
+              }`}
               src="/main_assets/png/Albums-img-2.png"
               alt="albums"
             />
           </div>
         </div>
 
-        <PageTitle title={t('photos')} paddingLeft={143} />
+        <PageTitle
+          isMobile={isMobile}
+          title={t("photos")}
+          paddingLeft={isMobile ? 50 : 143}
+        />
 
-        <div className={style.albumsSlider}>
+        <div
+          className={`${style.albumsSlider} ${
+            isMobile ? style.albumsSliderMob : ""
+          }`}
+        >
           {loading ? (
             <Loader />
           ) : (
-            <div className="albums-slider">
-              <AlbumsSlider
-                albums={albums}
-              />
-            </div>
+            <>
+              {!isMobile ? (
+                <div className="albums-slider">
+                  <AlbumsSlider albums={albums} />
+                </div>
+              ) : (
+                <>
+                  <div className={style.albumsContainer}>
+                    {albums.map((album: Album) => (
+                      <div key={album.id} className={style.album}>
+                        <Link
+                          href={`/gallery/albums/${album.id}`}
+                          legacyBehavior
+                        >
+                          <div className={style.overlay}>
+                            <h3 className={style.albumTitle}>{album.name}</h3>
+                          </div>
+                        </Link>
+
+                        <img
+                          className={style.albumImg}
+                          src={`${BASE_URL}${album.albumItems[0]?.albumPhotos[0]?.photoURL}`}
+                          alt={`Slide ${album.albumItems[0]?.albumPhotos[0]?.name} ${album.albumItems[0]?.albumPhotos[0]?.id}`}
+                        />
+                      </div>
+                    ))}
+                  </div>
+                </>
+              )}
+            </>
           )}
         </div>
       </div>
--- pages/gallery/albums.tsx
+++ pages/gallery/albums.tsx
@@ -1,6 +1,7 @@
 import Albums from "@/components/albums/Albums";
+import MobileContext from "@/contexts/MobileContext";
 import { NextPage } from "next";
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import getAlbums from "../api/albumsApi";
 import { Album } from "../models/album.interface";
 import { AlbumType } from "../models/albumType.enum";
@@ -8,6 +9,7 @@ import { AlbumType } from "../models/albumType.enum";
 const AlbumsPage: NextPage = () => {
   const [albums, setAlbums] = useState<Album[]>([]);
   const [loading, setLoading] = useState(true);
+  const { isMobile } = useContext(MobileContext);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -27,7 +29,7 @@ const AlbumsPage: NextPage = () => {
     fetchData();
   }, []);
 
-  return <Albums albums={albums} loading={loading} />;
+  return <Albums isMobile={isMobile} albums={albums} loading={loading} />;
 };
 
 export default AlbumsPage;
