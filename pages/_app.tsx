271bcccff8705c4af3cf4067fc9c6fbf6ab54149
Lasha <lashaiakobadze98@gmail.com>
2023-10-23 23:03:29 +0400

    add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,22 @@
 import Header from "@/layout/header/Header";
 import Footer from "@/layout/footer/Footer";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 import "@/styles/globals.css";
 import { LanguageProvider } from "@/contexts/LanguageContext";
 import { MobileProvider } from "@/contexts/MobileContext";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <LanguageProvider>
-      <MobileProvider>
-        <Header />
-        <Component {...pageProps} />
-        <Footer />
-      </MobileProvider>
-    </LanguageProvider>
+    <>
+      <LanguageProvider>
+        <MobileProvider>
+          <Header />
+          <Component {...pageProps} />
+          <Footer />
+        </MobileProvider>
+      </LanguageProvider>
+      <Analytics />
+    </>
   );
 }
